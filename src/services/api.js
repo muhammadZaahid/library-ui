0d7454cf8c89e8684ea9b0d8fc0fd5ffc72ce37a
Zaahid <mzra@ic.sg>
2025-07-10 16:36:38 +0700

    Update API call

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+export default axios.create({
+  baseURL: 'http://localhost:8080/api',
 });
-
-export default api;
